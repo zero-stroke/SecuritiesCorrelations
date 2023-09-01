04f6d1f0dba47ac26fa7f4d97ae8e5314b83343b
zayricch <57886710+zayricch@users.noreply.github.com>
2023-09-01 17:22:38 -0400

    Added input and dropdown boxes separate

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+ALPACA_BASE_URL = 'https://paper-api.alpaca.markets'
+
+PROJECT_ROOT = Path("C:/Users/afshin/IdeaProjects/apca_api_trader/")
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / 'data'
+STOCKS_DIR = DATA_DIR / 'Stock_data'
+FRED_DIR = DATA_DIR / 'FRED'
+
+
